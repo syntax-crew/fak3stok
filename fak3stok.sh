0987bdc6b1272ba9162b54bc213f16b56c915319
syntax-crew <syntaxerrorcrew.id@gmail.com>
2019-06-06 15:10:13 +0700

    Create fak3stok.sh

---
+++ fak3stok.sh
@@ -0,0 +1,40 @@
+! usr/bin/bash
+#Hapus Copyright? Lu Bukan Coder Bro!
+#syntax-crew opmem, contact +62 888-0928-0542
+
+clear
+
+figlet fak3stok.v1
+echo "(+)-----------------------------------(+)"
+echo " |   Simpel Tools, Hanya Main Logika   |"
+echo "(+)-----------------------------------(+)"
+echo " | Team : Syntax Error Crew            |"
+echo " | Excellent : AnonCyberTeam           |"
+echo " | TEH Squad Cyber - Security Darknet  |"
+echo "(+)-----------------------------60619-(+)"
+sleep 3
+echo " | Tanpa HTTPS/HTTP/WWW                |"
+read -p " | Masukan Website         : " web;
+read -p " | Masukan Subdomain Bebas : " sub;
+echo "(+)-----------------------------------(+)"
+sleep 3
+echo " |            HASILNYA BOS             |"
+echo "(+)-----------------------------------(+)"
+echo " http://$sub-1.$web"
+echo " http://$sub-2.$web"
+echo " http://$sub-3.$web"
+echo " http://$sub-4.$web"
+echo " http://$sub-5.$web"
+echo " http://$sub-6.$web"
+echo " http://$sub-7.$web"
+echo " http://$sub-8.$web"
+echo " http://$sub-9.$web"
+echo " http://$sub-10.$web"
+echo " http://$sub-11.$web"
+echo " http://$sub-12.$web"
+echo " http://$sub-13.$web"
+echo " http://$sub-14.$web"
+echo " http://$sub-15.$web"
+echo "(+)-----------------------------------(+)"
+echo " |  ~  SILAHKAN KAMU MIRROR KAN :3 ~   |"
+echo "(+)-----------------------------------(+)"
